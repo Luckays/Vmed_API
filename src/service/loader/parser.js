92f0748b7a8981e25329f8325e6f813abcd86a06
luckays <lukasbeloch@seznam.cz>
2020-05-11 16:17:48 +0100

    Fix_3

--- src/service/loader/parser.js
+++ src/service/loader/parser.js
@@ -9,7 +9,9 @@ import {
 export function parseFile(fullPath, bytes) {
     const extension = path.extname(fullPath).split('.')[1].toLocaleLowerCase();
     if (fullPath === '/nasdat/01/DATA/METEOgopeARCHIV/aktual.txt') return;
-    if (extension ==='met' || extension === 'tst' ) return;
+    if (extension ==='met' || extension === 'tst' ||extension==='puv'||extension==='res') return;
+  //  if (extension=== 'err' ||extension === 'exe') return;
+
     const readInterface = readline.createInterface({
         input: fs.createReadStream(fullPath),
         console: false,
