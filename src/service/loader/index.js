63b17492cb86ace02f7bf3970a3b3eba2b37c2e3
David Vurbs <davidvurbs@gmail.com>
2020-05-12 12:42:44 +0200

    Update index.js
    async/await v watcheru

--- src/service/loader/index.js
+++ src/service/loader/index.js
@@ -1,27 +1,44 @@
 import fs from 'fs';
 import { fetchSingleImportedFile } from '../../repositories/importedFileRepository';
 import { parseFile } from './parser';
+
 //old function like openFolder - first upload all files, then set refresh interval
-export async function  watchFolders(folders = [], interval) {
-    await folders.forEach(checkFolder);
-    setInterval(() => folders.forEach(checkFolder), interval);
+export async function watchFolders(folders = [], interval) {
+    do {
+        await folders.forEach(checkFolder);
+        await wait(5 * 1000)
+    } while (true);
 }
+
+async function wait(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
+
 //old function foldername - get filenames from folder
 async function checkFolder(folder) {
-    console.log(`📸 I am checking folder ${folder}`);
     const filenames = fs.readdirSync(folder);
+    const count = filenames.length;
+    console.log(`📸 I am checking folder ${folder}, ${count} files`);
+    
     while (filenames.length > 0) {
         const filename = filenames.shift();
         await checkFile(folder, filename);
+        
+        process.stdout.clearLine(0);
+        process.stdout.cursorTo(0);
+        process.stdout.write(`📸📸 Checked ${count - filenames.length}/${count} files from ${folder}`);
     }
+        
+    process.stdout.write('\n');
 }
+
 //- check if is in folder new file or some file change size
 async function checkFile(folder, filename) {
     const fullPathname = `${folder}/${filename}`;
     const bytes = fs.statSync(fullPathname).size;
     const row = await fetchSingleImportedFile(fullPathname);
-    console.log(fullPathname)
+    
     if (row === undefined || row.filesize < bytes) {
-        parseFile(fullPathname, bytes);
+        await parseFile(fullPathname, bytes);
     }
 }
