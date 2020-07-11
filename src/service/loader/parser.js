fcadecaef1e59d29411ca02c059afd89d1cfb10d
luckays <lukasbeloch@seznam.cz>
2020-07-11 16:21:34 +0100

    V_3

--- src/service/loader/parser.js
+++ src/service/loader/parser.js
@@ -8,11 +8,11 @@ import {
 
 export async function parseFile(fullPath, bytes,folder) {
     return new Promise(resolve => {
+        if (fullPath === '/nasdat/01/DATA/METEOgopeARCHIV/CSV') return resolve();
         const extension = path.extname(fullPath).split('.')[1].toLocaleLowerCase();
         switch (folder) {
             case '/nasdat/01/DATA/METEOgopeARCHIV':
                 if (fullPath === '/nasdat/01/DATA/METEOgopeARCHIV/aktual.txt') return resolve();
-                if (fullPath === '/nasdat/01/DATA/METEOgopeARCHIV/CSV') return resolve();
                 if (fullPath === '/nasdat/01/DATA/METEOgopeARCHIV/21810221.BGL') return resolve();
                 if (fullPath === '/nasdat/01/DATA/METEOgopeARCHIV/20200720.VGL') return resolve();
                 switch (extension) {
