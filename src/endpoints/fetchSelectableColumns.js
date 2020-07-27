413209b1957e748ce5033ed7ba710d0954338f0e
luckays <lukasbeloch@seznam.cz>
2020-07-27 10:21:45 +0100

    V_3

--- src/endpoints/fetchSelectableColumns.js
+++ src/endpoints/fetchSelectableColumns.js
@@ -22,11 +22,11 @@ export function fetchSelectableColumns(req, res) {
                 data: rows
                     .map((c) => c.Field)
                     .filter(
-                        (c) => (
+                        (c) =>
                             c !== 'datum' &&
+                            c !== 'dday' &&
                             c !== 'date_day' &&
-                            c !== 'day_time' &&
-                            c !== 'dday')
+                            c !== 'day_time'
                     ),
             });
     });
