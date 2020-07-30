5ee4e5a95159bf1eba9ac1c98522053fc3f70afd
luckays <lukasbeloch@seznam.cz>
2020-07-30 16:14:27 +0100

    V_4

--- src/repositories/selectableTablesRepository.js
+++ src/repositories/selectableTablesRepository.js
@@ -3,7 +3,7 @@ import { getConnection } from '../service/database';
 export function fetchAllSelectableTables() {
     return new Promise((resolve) => {
         getConnection().query(
-            'select * from selectable_tables',
+            'select * from selectable_tables order by title',
             [],
             (err, rows) => {
                 if (err) throw err;
