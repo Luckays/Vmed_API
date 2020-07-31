5d4dca6f035bb4aaf9a989fe15f85dfc27c61a25
luckays <lukasbeloch@seznam.cz>
2020-07-31 11:04:53 +0100

    V_4

--- index.js
+++ index.js
@@ -2,12 +2,10 @@ import { config } from 'dotenv';
 import { createServer } from './src/server';
 import { watchFolders } from './src/service/loader';
 import { invokeConnection } from './src/service/database';
-import * as Sentry from '@sentry/node'
 
 
 (async function () {
     config();
-    Sentry.init({ dsn: 'https://cc89270695804a59be08db183c9b938d@o428131.ingest.sentry.io/5373154' });
     await invokeConnection();
     createServer(4840);
         watchFolders(process.env.FOLDERS.split(','), process.env.EXCLUDED.split(','),process.env.RINEX.split(','), 5 * 1000);
