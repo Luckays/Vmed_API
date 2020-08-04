a9ebd1be786ded0dcf77ef10ffa1aa14f5c6820f
luckays <lukasbeloch@seznam.cz>
2020-08-04 11:25:09 +0100

    V_4

--- src/server.js
+++ src/server.js
@@ -19,11 +19,12 @@ export function createServer(port) {
     const app = express();
     app.use(bodyParser.json());
     app.use(cors());
+    console.log('ahoj')
 
     app.post('/data', fetchData);
     app.post('/show_data', fetchDataAnalysis);
     app.post('/download', fetchDataDownload);
-
+console.log('ahoj vy')
     app.get('/tables', fetchSelectableTables);
     app.post('/columns', fetchSelectableColumns);
     app.post('/show_data_day', fetchDayData);
@@ -32,6 +33,6 @@ export function createServer(port) {
     app.post('/show_data_day_var', fetchDayDataVar);
     app.post('/download_day', fetchDayDataDownload);
 
-    app.listen(port); 
+    app.listen(port);
     console.log(`Server is running on port ${port}`);
 }
