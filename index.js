ce187d54e4b4a0c0f6aef7939cd6e529ab15a433
luckays <lukasbeloch@seznam.cz>
2020-08-05 16:42:38 +0100

    V_4

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import { invokeConnection } from './src/service/database';
 (async function () {
     config();
     await invokeConnection();
+    console.log("ahoj")
     createServer(4840);
       //  watchFolders(process.env.FOLDERS.split(','), process.env.EXCLUDED.split(','),process.env.RINEX.split(','), 5 * 1000);
     })();
